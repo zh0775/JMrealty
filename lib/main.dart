dc9cb949a68a8bc9fd834ad242ce2bd697452b08
张涵 <zh0775@sina.cn>
2020-11-07 23:05:31 +0800

    222

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Home/JMHomeBanner.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,28 +9,75 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(home: HomeBodyWidget());
   }
 }
 
-class Home extends StatelessWidget {
+class HomeBodyWidget extends StatefulWidget {
+  @override
+  _HomeBodyWidgetState createState() => _HomeBodyWidgetState();
+}
+
+class _HomeBodyWidgetState extends State<HomeBodyWidget> {
+  List<BannerItem> bannerList = [];
+
+  @override
+  void initState() {
+    BannerItem item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/64/w1024h640/20181024/wBkr-hmuuiyw6863395.jpg''',
+        '''近日，北大全校教师干部大会刚刚召开，63岁的林建华卸任北大校长；原北大党委书记郝平接替林建华，成为新校长。曾在北京任职多年、去年担任山西高院院长的邱水平回到北大，担任党委书记。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学校长林建华（右）与时任北京大学党委书记郝平（左）''');
+    bannerList.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/99/w1024h675/20181024/FGXD-hmuuiyw6863401.jpg''',
+        '''邱水平、郝平、林建华均为“老北大人”，都曾离开北大，又重归北大任职。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学党委书记郝平讲话''');
+    bannerList.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/107/w1024h683/20181024/kZj2-hmuuiyw6863420.jpg''',
+        '''此番卸任的林建华，亦是北大出身，历任重庆大学、浙江大学、北京大学三所“双一流”高校校长。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学校长林建华讲话。''');
+    bannerList.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/105/w1024h681/20181024/tOiL-hmuuiyw6863462.jpg''',
+        '''书记转任校长的郝平，为十九届中央委员会候补委员。从北大毕业后留校，后离开北大，历任北京外国语大学校长、教育部副部长。2016年12月，时隔11年，郝平再回北大，出任北大党委书记。''');
+    bannerList.add(item);
+    super.initState();
+  }
+
+  double widgetHeight = 180.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('金慕'), centerTitle: true, actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            Icons.mail,
+        appBar: AppBar(title: Text('金慕'), centerTitle: true, actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 80),
+            child: IconButton(
+              icon: Icon(
+                Icons.mail,
+              ),
+              tooltip: '搜索',
+              onPressed: () {
+                print('click message');
+              },
+            ),
           ),
-          tooltip: '搜索',
-          onPressed: clickMessage,
-        ),
-      ]),
-    );
-  }
-
-  int count = 0;
-  void clickMessage() {
-    print('messageClick${count++}');
+        ]),
+        body: Container(
+            key: Key('333'),
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(color: Colors.grey),
+            child: Container(
+              key: Key('123'),
+              height: 100,
+              color: Colors.red,
+            )
+            // BannerWidget(
+            //   widgetHeight,
+            //   bannerList,
+            //   bannerPress: (post, item) {
+            //     print('window.innerHeight == ${MediaQuery.of(context).size}');
+            //     print('post === $post --- item === $item');
+            //   },
+            // ),
+            ));
   }
 }
