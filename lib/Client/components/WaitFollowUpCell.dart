1f3f6872674e821f206f93ff7bb708ad387d9177
张涵 <zh0775@sina.cn>
2020-11-20 18:09:06 +0800

    '客户cell'

--- lib/Client/components/WaitFollowUpCell.dart
+++ lib/Client/components/WaitFollowUpCell.dart
@@ -5,6 +5,16 @@ import 'package:flutter/material.dart';
 enum waitFollowLevel { A, B, C }
 
 class WaitFollowUpCell extends StatefulWidget {
+  // final waitFollowLevel level;
+  // final String name;
+  // final String sex;
+  // final String houseType;
+  // final String roomCount;
+  // final String houseSize;
+  // final String housePrice;
+  // final String newFollowTime;
+  // final String clientIntention;
+  // final String clientPhoneNum;
   @override
   _WaitFollowUpCellState createState() => _WaitFollowUpCellState();
 }
@@ -13,13 +23,24 @@ class _WaitFollowUpCellState extends State<WaitFollowUpCell> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
+    double cellHeight = 200;
+    double cellWidth = SizeConfig.screenWidth;
     return Container(
-      width: 100,
-      height: 200,
+      width: cellWidth,
+      height: cellHeight,
       decoration: BoxDecoration(
           border: Border(
         bottom: BorderSide(color: jm_line_color, width: 0.5),
       )),
+      child: Column(
+        children: [
+          Container(
+            height: 50,
+            width: cellWidth,
+            color: Colors.red,
+          )
+        ],
+      ),
     );
   }
 }
